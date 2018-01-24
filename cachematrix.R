69f83b4a3f1d54dd823946e782fdfe8beba4b76d
Schanze <denny.schanze@arcadis.nl>
2018-01-24 17:08:51 +0100

    first version of my cachematrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera R Programming
+## Programming assignment 2
+## Write an R function that is able to cache potentially time-consuming
+## computations. In this case we want to compute the inverse of a square matrix.
+## For this assignment, assume that the matrix supplied is always invertible.
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function creates a special "matrix" object that can
+## cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+        im <- NULL
+        set <- function(y) {
+                ## check if the matrix to be set identical to the one in the
+                ## cache. If so the cache will not be cleaned.
+                if(!identical(x,y)) {
+                        x <<- y
+                        im <<- NULL
 
+                } else {
+                        message("identical matrix")
+                }
+        }
+        get <- function() x
+        setinverse <- function(solve) im <<- solve
+        getinverse <- function() im
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: This function computes the inverse of the special "matrix"
+## returned by makeCacheMatrix above. If the inverse has already been
+## calculated (and the matrix has not changed), then cacheSolve should
+## retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        im <- x$getinverse()
+        if(!is.null(im)) {
+                message("getting cached data")
+                return(im)
+        }
+        data <- x$get()
+        im <- solve(data, ...)
+        x$setinverse(im)
+        im
 }
